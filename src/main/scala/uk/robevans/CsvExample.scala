41dafdab6f00e4945f42d4dfcaaff457cbca52d0
Rob Evans <rwevans1984@gmail.com>
2018-08-17 13:04:40 +0100

    Enable CsvExample to work as a stand alone locally running application

--- src/main/scala/uk/robevans/CsvExample.scala
+++ src/main/scala/uk/robevans/CsvExample.scala
@@ -1,39 +1,76 @@
 package uk.robevans
 
-import org.apache.spark.sql.{Dataset, SparkSession}
-import org.apache.spark.sql.types.StructType
+import java.io.File
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+
+import scala.io.Source._
 
 object CsvExample {
 
+  private var localFilePath: File = new File(ClassLoader.getSystemResource("test.csv").toURI())
+  private var dfsDirPath: String = ClassLoader.getSystemResource("").toURI().toString()
+
+  private def readFile(filename: String): List[String] = {
+    val lineIter: Iterator[String] = fromFile(filename).getLines()
+    val lineList: List[String] = lineIter.toList
+    lineList
+  }
+
+  def runLocalWordCount(fileContents: List[String]): Int = {
+    fileContents.flatMap(_.split(" "))
+      .flatMap(_.split("\t"))
+      .filter(_.nonEmpty)
+      .groupBy(w => w)
+      .mapValues(_.size)
+      .values
+      .sum
+  }
+
+  def overwriteAnyExistingFile(dfsFilename: String, fileRDD: RDD[String]) = {
+    val filePath = new File(dfsFilename).getAbsolutePath()
+    Runtime.getRuntime.exec(s"rm ${filePath}")
+    fileRDD.saveAsTextFile(dfsFilename)
+  }
+
   def main(args: Array[String]): Unit = {
 
+    val fileContents = readFile(localFilePath.toString())
+    val localWordCount = runLocalWordCount(fileContents)
+
+    println("Creating SparkSession")
     val spark = SparkSession
       .builder
-      .appName("StructuredNetworkWordCount")
+      .appName("DFS Read Write Test")
+      .config("spark.master", "local")
       .getOrCreate()
 
-    import spark.implicits._
+    println("Writing local file to DFS")
+    val dfsFilename = s"$dfsDirPath/dfs_read_write_test"
+    val fileRDD: RDD[String] = spark.sparkContext.parallelize(fileContents)
+    overwriteAnyExistingFile(dfsFilename, fileRDD)
 
-    val userSchema = new StructType().add("one", "double")
-    val csvDF = spark
-      .readStream
-      .option("header", "true")
-      .option("sep", ",")
-      .schema(userSchema)
-      .csv("resources")
+    println("Reading file from DFS and running Word Count")
+    val readFileRDD = spark.sparkContext.textFile(dfsFilename)
 
-    val dataset: Dataset[Double] = csvDF.as[Double]
+    val dfsWordCount = readFileRDD
+      .flatMap(_.split(" "))
+      .flatMap(_.split("\t"))
+      .filter(_.nonEmpty)
+      .map(w => (w, 1))
+      .countByKey()
+      .values
+      .sum
 
-    dataset.createOrReplaceTempView("numbers")
-    val wordsResult = spark.sql("select one from numbers") // returns another streaming DF
+    spark.stop()
 
-    val query = wordsResult.writeStream
-      .outputMode("append")
-      .format("parquet")
-      .option("checkpointLocation", "checkpoint")
-      .option("path", "output")
-      .start()
-
-    query.awaitTermination()
+    if (localWordCount == dfsWordCount) {
+      println(s"Success! Local Word Count $localWordCount and " +
+        s"DFS Word Count $dfsWordCount agree.")
+    } else {
+      println(s"Failure! Local Word Count $localWordCount " +
+        s"and DFS Word Count $dfsWordCount disagree.")
+    }
   }
 }
